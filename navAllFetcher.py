4b562180547661e9529ec9df2fac68693b9a6193
santoshbag <santosh.bag@gmail.com>
2018-05-16 16:32:55 +0530

    added fund house and scheme type columns added sql push to mf_nav_history database

--- navAllFetcher.py
+++ navAllFetcher.py
@@ -8,10 +8,14 @@ import datetime,csv
 import urllib.request
 from pandas import DataFrame
 from collections import deque
+from sqlalchemy import create_engine
+
 
 NAV_URL = "https://www.amfiindia.com/spages/NAVAll.txt?t=09082017092931"
 mfNavHistory_path = "F:\Mrig Analytics\Development\data\mfNAVAllHistory.csv"
 mfNavHistory = open(mfNavHistory_path,"a+")
+
+engine = create_engine('postgresql+psycopg2://postgres:xanto007@localhost:5432/RB_WAREHOUSE')
 
 def get_last_row(csv_filename,lines=1):
     with open(csv_filename,'r') as f:
@@ -34,19 +38,40 @@ except:
 if last_download_date != timestamp.strftime("%x"):
     written = False
     navs = []
-    navs_cols = ['Date','ISIN Div Payout/ ISIN Growth','ISIN Div Reinvestment','Scheme Name','Net Asset Value','Repurchase Price','Sale Price','Time Stamp']
+    navs_cols = ['Date','Fund House','Scheme Type','ISIN Div Payout/ ISIN Growth','ISIN Div Reinvestment','Scheme Name','Net Asset Value','Repurchase Price','Sale Price','Time Stamp']
+    mutual_fund_scheme_type = ""
+    mutual_fund_house = ""
+    sub_header_count = 0
     for line in data:
         line =  str(line,'utf-8')#+";"+timestamp.strftime("%x-%X")
         line = line.split(";")
         if len(line) > 1:
+            sub_header_count = 0
             try:
                 current_date = line[7].split("\r\n",1)[:-1][0]
-                navs.append(line[7].split("\r\n",1)[:-1]+line[1:-1]+[timestamp.strftime("%x-%X")])
+                navs.append(line[7].split("\r\n",1)[:-1]+[mutual_fund_house,mutual_fund_scheme_type]+line[1:-1]+[timestamp.strftime("%x-%X")])
                 written= True
             except:
                 pass
+        else:
+            if line[0].strip():    
+                sub_header_count = sub_header_count + 1
+                #print("sub_header_count "+ str(sub_header_count))
+                if sub_header_count == 2:
+                    mutual_fund_scheme_type = mutual_fund_house
+                    mutual_fund_house= line[0]
+                    #print("mutual_fund_scheme_type ->" + mutual_fund_scheme_type)
+                    sub_header_count = 0
+                else:
+                    mutual_fund_house= line[0]
+                    #print("mutual_fund_house -> " + mutual_fund_house)
     if written:
         navs = DataFrame(navs[1:],columns=navs_cols)
         navs.to_csv(mfNavHistory,index=False,header=headerAbsent)
+        navs = navs.drop('Time Stamp',axis=1)
+        try:
+            navs.to_sql('mf_nav_history',engine, if_exists='append', index=False)
+        except:
+            pass
         #mfNavHistory.write("\n<End>\n")
 mfNavHistory.close()
