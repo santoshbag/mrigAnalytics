a88fb0d25ba5511e21438290952ce9b6bd634a2a
santoshbag <santosh.bag@gmail.com>
2018-06-13 10:56:30 +0530

    reorganised functions and classes

--- instruments/termstructure.py
+++ instruments/termstructure.py
@@ -4,11 +4,13 @@ Created on Mon May 28 15:35:35 2018
 
 @author: Santosh Bag
 """
-
+import sys,os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import QuantLib as ql
 import mrigutilities
 import pandas as pd
+import instruments.qlMaps as qlMaps
 
 class TermStructure:
     """
@@ -26,7 +28,53 @@ class YieldCurveTermStructure(TermStructure):
     """
     def __init__(self,reference_date):
         self.reference_date = reference_date
+        self.curve = None
+
+    def setShift(self,date_spread_vector):
+        """
+        This function shifts the spot curve based on the spread
+        For parallel shifts to yield curve, the date_spread_vector is just the [spread]
+        
+        For non-parallel shifts, the date_spread_vector is a 2D array of dates and spreads
+        """
+        
+        #Check if the shift is parallel or non-parallel
+        parallel = True
+        if len(date_spread_vector) > 1:
+            parallel = False
+        
+        curveHandle = self.getCurveHandle()
+        if parallel:
+            spread_quote = ql.SimpleQuote(date_spread_vector[0][0])
+            spread_handle = ql.QuoteHandle(spread_quote)
+            
+            self.shiftedCurve = ql.ZeroSpreadedTermStructure(curveHandle,spread_handle)
+        else:
+            spread_handles = [ql.QuoteHandle(ql.SimpleQuote(s)) for s in date_spread_vector[1]]
+            dateList = [ql.Date(dt.day,dt.month,dt.year) for dt in date_spread_vector[0]]
+            self.shiftedCurve = ql.SpreadedLinearZeroInterpolatedTermStructure(curveHandle,
+                                                                          spread_handles,
+                                                                          dateList)
+        self.curve = self.shiftedCurve
+        return self.shiftedCurve
     
+    def getShiftedCurveHandle(self):
+        
+        shiftedCurveHandle = ql.YieldTermStructureHandle(self.shiftedCurve)
+        
+        return shiftedCurveHandle    
+    
+    def getCurve(self):
+        
+        return self.curve
+
+    def getCurveHandle(self):
+        
+        curve = self.getCurve()
+        curveHandle = ql.YieldTermStructureHandle(curve)
+        
+        return curveHandle
+
     
 class SpotZeroYieldCurve(YieldCurveTermStructure):
     """
@@ -55,6 +103,8 @@ class SpotZeroYieldCurve(YieldCurveTermStructure):
                                  self.interpolation,
                                  self.compounding,
                                  self.compounding_frequency)
+        self.curve = self.spotCurve
+
         
     def setupCurve(self,setupparams):
         try:
@@ -66,6 +116,7 @@ class SpotZeroYieldCurve(YieldCurveTermStructure):
         self.day_count = setupparams['day_count']
         self.calendar = setupparams['calendar']
         self.interpolation = setupparams['interpolation']
+        self.shiftparameter = setupparams['shiftparameter']
         
         ql.Settings.instance().evaluationDate = ql.Date(self.reference_date.day,
                                                         self.reference_date.month,
@@ -77,6 +128,10 @@ class SpotZeroYieldCurve(YieldCurveTermStructure):
                                  self.interpolation,
                                  self.compounding,
                                  self.compounding_frequency)
+        self.curve = self.spotCurve
+        
+        if self.shiftparameter[0][0] != None:
+            self.setShift(self.shiftparameter)
 
     def __setDBRates(self):
         sql = "select curvedate, tenor, yield from yieldcurve where curve = '"+self.currency+"' and curvedate='"+ self.reference_date.strftime('%Y-%m-%d')+"'"
@@ -92,9 +147,6 @@ class SpotZeroYieldCurve(YieldCurveTermStructure):
         spot_yields.insert(0,0.0)
         self.spot_rates = [spot_dates,spot_yields]        
         
-    def getCurve(self):
-        
-        return self.spotCurve
     #0.9846802113323603
     def getForwardRate(self,date1, 
                        date2, 
@@ -109,7 +161,7 @@ class SpotZeroYieldCurve(YieldCurveTermStructure):
         if frequency is None:
             frequency=self.compounding_frequency
         
-        forward_rate = self.spotCurve.forwardRate(ql.Date(date1.day,date1.month,date1.year),
+        forward_rate = self.curve.forwardRate(ql.Date(date1.day,date1.month,date1.year),
                                                   ql.Date(date2.day,date2.month,date2.year),
                                                   daycount,
                                                   compounding,
@@ -118,77 +170,54 @@ class SpotZeroYieldCurve(YieldCurveTermStructure):
         
     def getDiscountFactor(self,dates):
         
-        discount_factor = [self.spotCurve.discount(ql.Date(date.day,date.month,date.year)) for date in dates]
+        discount_factor = [self.curve.discount(ql.Date(date.day,date.month,date.year)) for date in dates]
         return discount_factor
     
-    def getCurveHandle(self):
+    def getSpotCurve(self):
         
-        spotCurve = self.getCurve()
-        spotCurveHandle = ql.YieldTermStructureHandle(spotCurve)
-        
-        return spotCurveHandle    
+        return self.spotCurve
     
-    def getShiftedCurve(self,date_spread_vector):
-        """
-        This function shifts the spot curve based on the spread
-        For parallel shifts to yield curve, the date_spread_vector is just the spread
+    def getSpotCurveHandle(self):
         
-        For non-parallel shifts, the date_spread_vector is a 2D array of dates and spreads
-        """
-        
-        #Check if the shift is parallel or non-parallel
-        parallel = True
-        if len(date_spread_vector) > 1:
-            parallel = False
-        
-        spotCurveHandle = self.getSpotCurveHandle()
-        if parallel:
-            spread_quote = ql.SimpleQuote(date_spread_vector[0])
-            spread_handle = ql.QuoteHandle(spread_quote)
-            
-            self.shiftedCurve = ql.ZeroSpreadedTermStructure(spotCurveHandle,spread_handle)
-        else:
-            spread_handles = [ql.QuoteHandle(ql.SimpleQuote(s)) for s in date_spread_vector[1]]
-            dateList = [ql.Date(dt.day,dt.month,dt.year) for dt in date_spread_vector[0]]
-            self.shiftedCurve = ql.SpreadedLinearZeroInterpolatedTermStructure(spotCurveHandle,
-                                                                          spread_handles,
-                                                                          dateList)
-        return self.shiftedCurve
+        spotCurveHandle = ql.YieldTermStructureHandle(self.spotCurve)
+        return spotCurveHandle
     
-    def getShiftedCurveHandle(self):
-        
-        shiftedCurveHandle = ql.YieldTermStructureHandle(self.shiftedCurve)
-        
-        return shiftedCurveHandle    
 
-
-class Volatilty(TermStructure):
+class FlatForwardYieldCurve(YieldCurveTermStructure):
     """
-    Volatility Class to encapsulate volatilty term structure, volatility surface
+    Flat Forward Yield class to generate constant yield term structure
     """
     
-    def __init__(self,reference_date,setupparams):
+    def __init__(self,reference_date,flat_rate):
         self.reference_date = reference_date     #Date for which the rates are applicable
+        self.flat_rate= flat_rate
         self.day_count = ql.Thirty360()
         self.calendar = ql.India()
         self.interpolation = ql.Linear()
+        ql.Settings.instance().evaluationDate = qlMaps.qlDates(reference_date)
 
     def setupCurve(self,setupparams):
-        self.spot_vols = setupparams['spot_vols'] #List, a 2D array of dates and spot rates
         self.day_count = setupparams['day_count']
         self.calendar = setupparams['calendar']
-        self.interpolation = setupparams['interpolation']     
-    
-    def getConstantVol(self,vol_quote):
-        vol_ts = ql.BlackConstantVol(self.reference_date,
-                                     self.calendar,
-                                     vol_quote,
-                                     self.day_count)
-        vol_ts_handle = ql.BlackVolTermStructureHandle(vol_ts)
+        self.compounding = setupparams['compounding']
+        self.compounding_frequency = setupparams['compounding_frequency']
+        self.shiftparameter = setupparams['shiftparameter']
+        self.flat_ts = ql.FlatForward(qlMaps.qlDates(self.reference_date),
+                                              self.flat_rate,
+                                              self.day_count,
+                                              self.compounding,
+                                              self.compounding_frequency)
         
-        return vol_ts_handle
+        self.curve = self.flat_ts
 
-class FlatForwardYieldCurve(YieldCurveTermStructure):
+        if self.shiftparameter[0][0] != None:
+            self.setShift(self.shiftparameter)
+        
+    def getCurve(self):
+        return self.flat_ts
+    
+    
+class FlatDividendYieldCurve(YieldCurveTermStructure):
     """
     Flat Forward Yield class to generate constant yield term structure
     """
@@ -203,17 +232,54 @@ class FlatForwardYieldCurve(YieldCurveTermStructure):
     def setupCurve(self,setupparams):
         self.day_count = setupparams['day_count']
         self.calendar = setupparams['calendar']
-        self.flat_ts = ql.FlatForward(ql.Date(self.reference_date.day,
-                                              self.reference_date.month,
-                                              self.reference_date.year),
+        self.compounding = setupparams['compounding']
+        self.compounding_frequency = setupparams['compounding_frequency']
+        self.flat_ts = ql.FlatForward(qlMaps.qlDates(self.reference_date),
                                               self.flat_rate,
                                               self.day_count,
-                                              ql.Compounded,
-                                              ql.Semiannual)
+                                              self.compounding,
+                                              self.compounding_frequency)
         self.flat_ts_handle = ql.YieldTermStructureHandle(self.flat_ts)
-        
+    
     def getCurve(self):
         return self.flat_ts
     
     def getCurveHandle(self):
         return self.flat_ts_handle
+    
+    
+
+class Volatilty(TermStructure):
+    """
+    Volatility Class to encapsulate volatilty term structure, volatility surface
+    """
+    def __init__(self,reference_date):
+        self.reference_date = reference_date     #Date for which the rates are applicable
+
+class FlatVolatilityCurve(Volatilty):
+    """
+    Flat Vol class to generate constant volatility term structure
+    """    
+    
+    def __init__(self,reference_date,flat_vol):
+        self.reference_date = reference_date     #Date for which the rates are applicable
+        self.flat_vol= flat_vol
+        self.day_count = ql.Thirty360()
+        self.calendar = ql.India()
+        self.interpolation = ql.Linear()
+
+    def setupCurve(self,setupparams):
+        self.spot_vols = setupparams['spot_vols'] #List, a 2D array of dates and spot rates
+        self.day_count = setupparams['day_count']
+        self.calendar = setupparams['calendar']
+        self.vol_ts = ql.BlackConstantVol(qlMaps.qlDates(self.reference_date),
+                                     self.calendar,
+                                     self.flat_vol,
+                                     self.day_count)
+        self.vol_ts_handle = ql.BlackVolTermStructureHandle(self.vol_ts)
+        
+    def getCurve(self):
+        return self.vol_ts
+    
+    def getCurveHandle(self):
+        return self.vol_ts_handle
