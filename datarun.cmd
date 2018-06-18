5f5eb1077eb46615b831981d3b999f23dd64547a
santoshbag <santosh.bag@gmail.com>
2018-06-18 16:54:20 +0530

    auto script updated

--- datarun.cmd
+++ datarun.cmd
@@ -1,15 +1,20 @@
 @echo off
 
-if [%1] == [] goto usage
-if [%2] == [] goto usage
+set TODAY=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+
+for /f "usebackq" %%i in (`PowerShell $date ^= Get-Date^; $date ^= $date.AddDays^(-1^)^; $date.ToString^('yyyyMMdd'^)`) do set YESTERDAY=%%i 
+
+set startdate=%1
+set enddate=%2
+
+
+if [%1] == [] set startdate=%YESTERDAY%
+if [%2] == [] set enddate=%TODAY%
 
 echo Welcome to Mrig Data Run!
 @echo -------------Mrig Data Run----------- >> dailyBatchRunLog.txt
 @echo ------------%date% %time%------------>> dailyBatchRunLog.txt 
 cd /D F:\Mrig Analytics\Development\mrigAnalytics\
-
-set startdate=%1
-set enddate=%2
 
 
 rem python.exe test.py %startdate% %enddate%
@@ -37,4 +42,6 @@ exit /B 1
 @echo Batch file did not run
 @echo(
 @echo Usage : datarun.cmd startDate endDate : dates in YYYYMMDD format
+@echo(
+
 exit /B 1 
