76875ec51a24f34e3ce6a70f76ef1819e07ac518
santoshbag <santosh.bag@gmail.com>
2020-05-03 00:00:13 +0530

    fo_loader added the move utility

--- data/fo_loader.py
+++ data/fo_loader.py
@@ -11,6 +11,7 @@ sys.path.append(join(dirname(__file__), '..'))
 import pandas as pd
 #from datetime import datetime
 from os import listdir
+from shutil import move
 from mrigutilities import sql_engine
 
 
@@ -18,9 +19,9 @@ datadir = dirname(__file__)
 base = join(datadir, '..', '..', 'data','2020','Futures')  # ''F:\\Mrig Analytics\\Development\\data\\'
 print(base)
 
-onlyfiles = [join(base,f) for f in listdir(base) if isfile(join(base, f))]
+onlyfiles = [(join(base,f),join(base,'loaded',f)) for f in listdir(base) if isfile(join(base, f))]
 
-engine = sql_engine(dbhost='localhost')
+engine = sql_engine()
 colmap = { 'INSTRUMENT' : 'instrument', 
           'SYMBOL' :  'symbol', 
           'EXPIRY_DT' :  'expiry', 
@@ -37,8 +38,8 @@ colmap = { 'INSTRUMENT' : 'instrument',
           'TIMESTAMP' :  'date'}
 
 for fo_csv in onlyfiles:
-    fo_pd = pd.read_csv(fo_csv)
+    fo_pd = pd.read_csv(fo_csv[0])
     fo_pd = fo_pd[list(colmap.keys())]
     fo_pd = fo_pd.rename(columns=colmap)
     fo_pd.to_sql('option_history', engine, if_exists='append', index=False)
-    
+    move(fo_csv[0], fo_csv[1])
